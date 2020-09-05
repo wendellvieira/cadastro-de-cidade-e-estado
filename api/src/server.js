07afc6f5ccad239e021b9b2f5df04c05942f11a2
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-09-05 12:07:14 -0300

    configurando o cors e bodyparse

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,13 @@ module.exports = class Server {
     constructor(){
         const express = require('express')
         this.app = express()
-        
+
+        const bodyParser = require('body-parser')
+        this.app.use( bodyParser.json() )
+
+        const cors = require('cors')
+        this.app.use( cors() )
+
         this.port = process.env.API_INT_PORT || 5000
     }
 
