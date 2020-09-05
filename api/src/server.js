acecf0906fb672b929ca9c97d19dae3bdf17a4ac
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-09-05 12:00:32 -0300

    implementado a instancia do express

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,14 @@
-
-
 module.exports = class Server {
+    constructor(){
+        const express = require('express')
+        this.app = express()
+        
+        this.port = process.env.API_INT_PORT || 5000
+    }
+
     init(){
-        console.log("O Servidor estar online")
+        this.app.listen( this.port, () => {
+            console.log(`O Servidor estar online na porta ${this.port}` )
+        } )
     }
 }
