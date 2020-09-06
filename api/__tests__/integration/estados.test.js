9d263b29e6b55aea347ee45506b763282f512a42
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-09-06 13:40:59 -0300

    testes da api de estados

---
+++ api/__tests__/integration/estados.test.js
@@ -0,0 +1,93 @@
+const request = require("supertest")
+const Server = require("Src/server.js")
+const API = (new Server)
+
+describe("Integridade da api de estados", () => {
+    beforeAll(async () => {
+        await API.testInit()
+    })
+
+    let id_estado;
+    const Estado = {
+        nome: "Rio de Janeiro",
+        abreviacao: "RJ"        
+    }
+
+   
+    describe("POST /api/estados", () => {
+        let resp; 
+        let status;
+
+        beforeAll( async () => {
+            resp = await request( API.app )
+                .post('/api/estados')
+                .send(Estado)
+            
+            id_estado = resp.body._id
+        })
+
+        test("Verificar se o metodo retorna o status 200", () => {                 
+            expect(resp.statusCode).toBe(200)       
+        })
+        test("Verifica se os o registro está integro", () => {                 
+            expect(resp.body).toEqual( expect.objectContaining(Estado) )       
+        })
+    })
+
+    describe("GET /api/estados", () => {   
+             
+        test("Verifica o status:200 e se a resposta é array", async () => {        
+            const { statusCode, body } = await request( API.app ).get('/api/estados')
+            expect(statusCode).toBe(200)    
+            expect( Array.isArray(body) ).toBeTruthy()
+        })
+
+        test("Verifica se consigo pegar um unico registro", async () => {
+            const { statusCode, body } = await request( API.app ).get(`/api/estados/${id_estado}`)
+            expect(statusCode).toBe(200)  
+            expect( body ).toEqual( expect.objectContaining({ ...Estado, _id: id_estado }) )
+        })
+    })
+
+    describe("PUT /api/estados", () => {  
+        let resp;
+        const newState = {
+            nome: "São paulo",
+            abreviacao: "SP"
+        }
+        beforeAll( async () => {
+            resp = await request( API.app )
+                .put(`/api/estados/${id_estado}`)
+                send(newState)
+        }) 
+
+        test("Verificar se o metodo retorna o status 200", async () => {
+            expect(resp.statusCode).toBe(200)       
+        })
+        test("Verifica se o update foi realizado", async () => {
+            expect(resp.body).toEqual( expect.objectContaining({ ...newState,  _id: id_estado }) )       
+        })
+        test("Verifica se a data de alteração foi criada", async () => {
+            expect(!!resp.body.ultima_alteracao).toBeTruthy()    
+        })
+
+    })
+
+    describe("DELETE /api/estados", () => {    
+
+        test("Verificar se o metodo retorna o status 200", async () => { 
+            const resp = await request( API.app ).delete(`/api/estados/${id_estado}`)       
+            expect(resp.statusCode).toBe(200)       
+        })
+        
+        test("Verificar se o foi realmente excluido", async () => { 
+            const { statusCode } = await request( API.app ).get(`/api/estados/${id_estado}`)      
+            expect( statusCode ).toBe(404)       
+        })
+
+    })
+        
+    afterAll( (done) => {
+        API.disconnectMongo();
+    })
+})
