6d97d5d03bdabbf6ccef0c60de8148a1deca79ad
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-09-06 20:25:20 -0300

    implementações de estado

--- web/src/components/cadastro-estado.vue
+++ web/src/components/cadastro-estado.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        cadastro de estado
+    <div class='row mt-4'>
+        <div class="col-9">
+            <input type="text" placeholder="Nome" class="form-control">
+        </div>
+        <div class="col-3">
+            <input type="text" placeholder="UF" class="form-control">
+
+            <button class="btn btn-success btn-block mt-3">
+                Enviar
+            </button>
+        </div>
     </div>
 </template>
