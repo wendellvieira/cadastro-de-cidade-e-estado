8911b9f14aa78ab9bb789afffb8bc2360e3faad4
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-09-06 20:24:54 -0300

    implementações da cidade

--- web/src/components/cadastro-cidade.vue
+++ web/src/components/cadastro-cidade.vue
@@ -1,5 +1,17 @@
 <template>
-    <div>
-        cadastro de cidade
+    <div class='row mt-4 align-items-end'>
+        <div class="col-6">
+            <input type="text" placeholder="Nome" class="form-control">
+        </div>
+        <div class="col-6">
+            <input type="text" placeholder="UF" class="form-control">
+        </div>
+
+        <div class="col-9"></div>
+        <div class='mt-3 col-3'>
+            <button class="btn btn-success btn-block ">
+                Enviar
+            </button>
+        </div>
     </div>
 </template>
--- web/src/components/lista-cidades.vue
+++ web/src/components/lista-cidades.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        lista de cidades
+    <div class='mt-5'>
+        <table class="table table-striped table-bordered table-sm">
+            <thead>
+                <tr>
+                    <th>Cidade</th>
+                    <th>Estado</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Rio de Janeiro</td>
+                    <td>RJ</td>
+                    <td>
+                        <button class="btn btn-primary btn-sm">
+                            E
+                        </button>
+                        <button class="btn btn-danger btn-sm">
+                            D
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
